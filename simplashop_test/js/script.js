4f20cccf1babe978061aec7b2be3ad73840b01e3
Olena-Bulatova <bulatova853@gmail.com>
2020-10-11 23:42:40 +0300

    Add simplashop_test project

---
+++ simplashop_test/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.banner__slider').slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        autospeed: 1500,
+        waitForAnimate: false,
+    });
+
+});
